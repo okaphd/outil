b0c2923d6489a30685fdfeebc02888eba29dc445
aphd <aphidarium@proton.me>
2025-08-18 21:11:19 +0100

    work with dates

--- main.ts
+++ main.ts
@@ -11,23 +11,31 @@ export default class Utils extends Plugin {
 					if (text === "%PROPERTIES") {
 						let el = codeblock.createSpan({ cls: "ou-properties" });
 
+						console.log(context.frontmatter);
 						for (let i in context.frontmatter) {
 							let container = el.createSpan({ text: "", cls: "ou-properties-container" });
 							let key = container.createSpan({ text: "", cls: "ou-properties-key" });
-							key.innerHTML += i + " ";
+							key.innerHTML += i;
 
 							let field = container.createSpan({ text: "", cls: "ou-properties-field" });
-							for (let j in context.frontmatter[i]) {
-								let text = context.frontmatter[i][j];
+							if (!Array.isArray(context.frontmatter[i])) {
+								let text = context.frontmatter[i];
 								const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
-								console.log(link);
 
-								if (link) {
-									if (link[0]) {
-										text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
-									}
+								if (link && link[0]) {
+									text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
 								}
 								field.innerHTML += text;
+							} else {
+								for (let j in context.frontmatter[i]) {
+									let text = context.frontmatter[i][j];
+									const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
+
+									if (link && link[0]) {
+										text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
+									}
+									field.innerHTML += text;
+								}
 							}
 							field.innerHTML += " <br>";
 						}

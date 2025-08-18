6d66d0efa9cbba043957a0ab1013ffeb1917def6
aphd <aphidarium@proton.me>
2025-08-18 18:34:44 +0100

    rename to utils; md postprocessor

--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
 import { Plugin } from 'obsidian';
 
-export default class Nodes extends Plugin {
+export default class Utils extends Plugin {
 	async onload() {
+		this.registerMarkdownPostProcessor((element, context) => {
+			const codeblocks = element.findAll('code');
+
+			for (let codeblock of codeblocks) {
+				const text = codeblock.innerText.trim();
+				if (text[0] === '%') {
+					const el = codeblock.createSpan({
+						text: "PLACEHOLDER",
+					});
+					codeblock.replaceWith(el);
+				}
+			}
+		});
 	}
 
 	onunload() {

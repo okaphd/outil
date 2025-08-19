d49ecb30fcd0808201e866183bde92f917d13261
aphd <aphidarium@proton.me>
2025-08-19 23:33:51 +0100

    cleanup

--- main.ts
+++ main.ts
@@ -11,34 +11,38 @@ export default class Utils extends Plugin {
 					if (text === "%PROPERTIES") {
 						let el = codeblock.createSpan({ cls: "ou-properties" });
 
-						console.log(context.frontmatter);
 						for (let i in context.frontmatter) {
 							let container = el.createSpan({ text: "", cls: "ou-properties-container" });
 							let key = container.createSpan({ text: "", cls: "ou-properties-key" });
+							let field = container.createSpan({ text: "", cls: "ou-properties-field" });
 							key.innerHTML += i;
 
-							let field = container.createSpan({ text: "", cls: "ou-properties-field" });
-							if (!Array.isArray(context.frontmatter[i])) {
-								let text = context.frontmatter[i];
-								const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
-
-								if (link && link[0]) {
-									text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
-								}
-								field.innerHTML += text;
-							} else {
+							if (Array.isArray(context.frontmatter[i])) {
 								for (let j in context.frontmatter[i]) {
-									let text = context.frontmatter[i][j];
-									const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
-
-									if (link && link[0]) {
-										text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
-									}
-									field.innerHTML += text;
+									field.innerHTML += this.toHTML(context.frontmatter[i][j]);
 								}
+							} else {
+								field.innerHTML += this.toHTML(context.frontmatter[i]);
 							}
 							field.innerHTML += " <br>";
 						}
+
+						codeblock.replaceWith(el);
+					} else if (text === "%CHILDREN") {
+						let el = codeblock.createDiv({cls: "el-table"});
+						let table = el.createEl("table");
+
+						let thead = table.createEl("thead")
+						let tr = thead.createEl("tr")
+						tr.createEl("th", {text: "Note"});
+						tr.createEl("th", {text: "Date Created"});
+
+						let tbody = table.createEl("tbody")
+
+						tr = tbody.createEl("tr");
+						let t = tr.createEl("td", {text: ""});
+						t.innerHTML = this.toHTML("[[High-Level Programming Languages]]");
+						tr.createEl("td", {text: "2025-08-18"});
 
 						codeblock.replaceWith(el);
 					}
@@ -48,5 +52,16 @@ export default class Utils extends Plugin {
 	}
 
 	onunload() {
+	}
+
+	toHTML(input: string) {
+		let text = input;
+		const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
+
+		if (link && link[0]) {
+			text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
+		}
+
+		return text;
 	}
 }

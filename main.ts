67f8bb9ee7882281d1906c5a5c1480ffec0710b3
aphd <aphidarium@proton.me>
2025-08-20 11:43:15 +0100

    %children table w/ date metadata

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 import { Plugin } from 'obsidian';
 
+function toHTML(input: string) {
+	let text = input;
+	const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
+
+	if (link && link[0]) {
+		text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
+	}
+	return text;
+}
+
 export default class Utils extends Plugin {
 	async onload() {
 		this.registerMarkdownPostProcessor((element, context) => {
@@ -19,30 +29,54 @@ export default class Utils extends Plugin {
 
 							if (Array.isArray(context.frontmatter[i])) {
 								for (let j in context.frontmatter[i]) {
-									field.innerHTML += this.toHTML(context.frontmatter[i][j]);
+									field.innerHTML += toHTML(context.frontmatter[i][j]);
 								}
 							} else {
-								field.innerHTML += this.toHTML(context.frontmatter[i]);
+								field.innerHTML += toHTML(context.frontmatter[i]);
 							}
 							field.innerHTML += " <br>";
 						}
 
 						codeblock.replaceWith(el);
 					} else if (text === "%CHILDREN") {
-						let el = codeblock.createDiv({cls: "el-table"});
+						const children = this.app.vault.getMarkdownFiles().filter((tfile) => {
+							const fm = this.app.metadataCache.getFileCache(tfile).frontmatter
+							if (fm) {
+								if (fm.parents) {
+									return fm.parents.contains("[[" + this.app.workspace.getActiveFile().basename + "]]");
+								}
+							}
+							return false;
+						});
+
+						if (children.length == 0) {
+							codeblock.remove();
+							return;
+						}
+
+						const el = codeblock.createDiv({cls: "el-table"});
 						let table = el.createEl("table");
 
-						let thead = table.createEl("thead")
-						let tr = thead.createEl("tr")
-						tr.createEl("th", {text: "Note"});
-						tr.createEl("th", {text: "Date Created"});
+						let header = table.createEl("thead")
+						let row = header.createEl("tr")
+						row.createEl("th", {text: "Children"});
+						row.createEl("th", {text: "Date"});
 
-						let tbody = table.createEl("tbody")
+						let body = table.createEl("tbody")
 
-						tr = tbody.createEl("tr");
-						let t = tr.createEl("td", {text: ""});
-						t.innerHTML = this.toHTML("[[High-Level Programming Languages]]");
-						tr.createEl("td", {text: "2025-08-18"});
+						for (const c of children) {
+							row = body.createEl("tr");
+							let t = row.createEl("td", {text: ""});
+							t.innerHTML = toHTML("[[" + c.basename + "]]");
+							const fm = this.app.metadataCache.getFileCache(c).frontmatter;
+							let date_text = "-";
+							if (fm) {
+								if (fm.date) {
+									date_text = fm.date;
+								}
+							}
+							row.createEl("td", {text: date_text});
+						}
 
 						codeblock.replaceWith(el);
 					}
@@ -54,14 +88,4 @@ export default class Utils extends Plugin {
 	onunload() {
 	}
 
-	toHTML(input: string) {
-		let text = input;
-		const link = [...text.matchAll(new RegExp("(?<=\\[\\[).*(?=\\]\\])", "gm"))][0];
-
-		if (link && link[0]) {
-			text = `<a data-href="${link[0]}" href="${link[0]}" class="internal-link" target="_blank" rel="noopener nofollow">${link[0]}</a>`;
-		}
-
-		return text;
-	}
 }
